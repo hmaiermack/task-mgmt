7c9f5a7688c90508919972dfb9fa147d548664c8
Mack Maier <hmaiermack@gmail.com>
2022-02-12 16:03:05 -0800

    GetTasksFilterDto validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../task.model';
+import { IsEnum, IsString, IsOptional } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
